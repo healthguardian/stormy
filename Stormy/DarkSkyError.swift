9ed126843b53cca50b655e7e58c932fefecf1254
Maxence Roy <m@masserm.com>
2018-09-27 17:17:10 -0500

    JSONDownloader and Error Handling

---
+++ Stormy/DarkSkyError.swift
@@ -0,0 +1,16 @@
+//
+//  DarkSkyError.swift
+//  Stormy
+//
+//  Created by Maxence Roy on 9/27/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+enum DarkSkyError: Error {
+    case requestFailed
+    case responseFailed(statusCode: Int)
+    case invalidData
+    case jsonParsingFailed
+}
---
+++ Stormy/JSONDownloader.swift
@@ -0,0 +1,50 @@
+//
+//  JSONDownloader.swift
+//  Stormy
+//
+//  Created by Maxence Roy on 9/27/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+class JSONDownloader {
+    let session: URLSession
+    
+    init(configuration: URLSessionConfiguration) {
+        self.session = URLSession(configuration: configuration)
+    }
+    
+    convenience init() {
+        self.init(configuration: .default)
+    }
+    
+    typealias JSON = [String: AnyObject]
+    
+    func jsonTask(with request: URLRequest, completionHandler completion: @escaping (JSON?, DarkSkyError?) -> Void) -> URLSessionDataTask {
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(nil, .requestFailed)
+                return
+            }
+            
+            if httpResponse.statusCode == 200 {
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? JSON
+                        completion(json, nil)
+                    } catch {
+                        completion(nil, .jsonParsingFailed)
+                    }
+                } else {
+                    completion(nil, .invalidData)
+                }
+            } else {
+                completion(nil, .responseFailed(statusCode: httpResponse.statusCode))
+            }
+        }
+        
+        return task
+    }
+}
