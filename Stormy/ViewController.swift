f1e4c813540c85b7b7dbe3f315a0e0a4603e3561
Maxence Roy <m@masserm.com>
2018-09-27 13:30:42 -0500

    Initial Commit

---
+++ Stormy/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Pasan Premaratne on 5/8/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentSummaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let currentWeather = CurrentWeather(temperature: 85.0, humidity: 0.8, precipProbability: 0.1, summary: "Hot!", icon: "clear-day")
+        let viewModel = CurrentWeatherViewModel(model: currentWeather)
+        
+        displayWeather(using: viewModel)
+    }
+    
+    func displayWeather(using viewModel: CurrentWeatherViewModel) {
+        currentTemperatureLabel.text = viewModel.temperature
+        currentHumidityLabel.text = viewModel.humidity
+        currentPrecipitationLabel.text = viewModel.precipitationProbability
+        currentSummaryLabel.text = viewModel.summary
+        currentWeatherIcon.image = viewModel.icon
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
